57b7f2ff4b2b2ad2b6df9d22f6b8858a4f810e32
BJ-Ranjani <95627976+BJ-Ranjani@users.noreply.github.com>
2021-12-16 18:04:52 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,57 +1,18 @@
-    module "ec2" {
-  source        = "clouddrove/ec2/aws"
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
 
-    name        = "ec2"
-    environment = "test"
-    label_order = ["name", "environment"]
+  name = "single-instance"
 
-    #instance
-    instance_enabled = true
-    instance_count   = 2
-    ami              = "ami-08d658f84a6d84a80"
-    instance_type    = "t2.nano"
-    monitoring       = false
-    tenancy          = "default"
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
 
-    #Networking
-    vpc_security_group_ids_list = [module.ssh.security_group_ids, module.http-https.security_group_ids]
-    subnet_ids                  = tolist(module.public_subnets.public_subnet_id)
-    assign_eip_address          = true
-    associate_public_ip_address = true
-
-    #Keypair
-    key_name = module.keypair.name
-
-    #IAM
-    instance_profile_enabled = true
-    iam_instance_profile     = module.iam-role.name
-
-    #Root Volume
-    root_block_device = [
-      {
-        volume_type           = "gp2"
-        volume_size           = 15
-        delete_on_termination = true
-        kms_key_id            = module.kms_key.key_arn
-      }
-    ]
-
-    #EBS Volume
-    ebs_optimized      = false
-    ebs_volume_enabled = false
-    ebs_volume_type    = "gp2"
-    ebs_volume_size    = 30
-
-    #DNS
-    dns_enabled = false
-    dns_zone_id = "Z1XJD7SSBKXLC1"
-    hostname    = "ec2"
-
-    #Tags
-    instance_tags = { "snapshot" = true }
-
-    # Metadata
-    metadata_http_tokens_required        = "optional"
-    metadata_http_endpoint_enabled       = "enabled"
-    metadata_http_put_response_hop_limit = 2
-    }
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
