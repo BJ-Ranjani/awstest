2a3fbabb2723ded9a3d51e01e188f555c806c355
BJ-Ranjani <95627976+BJ-Ranjani@users.noreply.github.com>
2021-12-16 17:39:11 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,5 +1,18 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
 
-resource "aws_instance" "example" {
-     ami = ami-0509c15d6eb3fcaa4
-     instance_type = "t2.micro"
+  name = "single-instance"
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
